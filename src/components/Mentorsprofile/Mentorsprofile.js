359592e7e0e100975a5de0ca1d01c63585064102
User0017 <user0017@static-226-59-228-77.ipcom.comunitel.net>
2020-04-10 11:30:57 +0200

    working on mentorsprofile

--- src/components/Mentorsprofile/Mentorsprofile.js
+++ src/components/Mentorsprofile/Mentorsprofile.js
@@ -13,10 +13,24 @@ function Mentorsprofile() {
            <img align="left" className="fb-image-lg" src="https://www.solidbackgrounds.com/images/4096x2304/4096x2304-light-steel-blue-solid-color-background.jpg" alt="Profile image example"/>
            <img align="left" className="fb-image-profile thumbnail" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSf2PNJRuzk7SnUuq7OuOq77LQXU28j1YmyHS7FmgdYRfMg85rK&usqp=CAU" alt="Profile image example"/>
            <div className="fb-profile-text">
-               <h3>MENTOR ONE</h3>
-               <p>Software engineer</p>
-           </div>
-       </div>
+               <h4>Mentor One</h4>
+               <p>Software engineer</p>       
+            <ul className="nav nav-pills pull-right countlist" role="tablist">
+              <li role="presentation">
+                <h3>0<br/>
+                  <small>Mentoring Hours</small> </h3>
+              </li>
+              <li role="presentation">
+                <h3>0<br/>
+                  <small>1-2-1 Classes</small> </h3>
+              </li>
+              <li role="presentation">
+                <h3>0<br/>
+                  <small>Students</small> </h3>
+              </li>
+            </ul>
+          </div> 
+       </div>                  
    </div>
    </div>
     );
