dbc1c2bff40a4dac6245007c5b3e6174ff845ee4
rakibul4477 <rajon10nabia@icloud.com>
2020-04-11 11:41:51 +0200

    new page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import Nav from './components/NavBar/nav';
 import Carousel from './components/Carousel/Carousel';
 import Calendar from './components/Calendar/Calendar';
@@ -7,39 +13,48 @@ import Testimonials from './components/Students/Testimonials';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import './App.css';
-
-
+import ContactPage from './pages/Contact';
+import './pages/Contact.css';
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.handleDayClick=this.handleDayClick.bind(this);
-    this.state={
+    this.handleDayClick = this.handleDayClick.bind(this);
+    this.state = {
       selectedDate: new Date()
     }
   }
-  render(){
-    const {selectedDate}=this.state;
-    return(
-      <div className="App">
-        <div className="Navigation"><Nav/></div>
-        <div className="Carousel"><Carousel/></div>
-        <div className="Calendar"><Calendar fullDate={selectedDate} onDayClick={this.handleDayClick}/></div>
-        <div className="MentorCards"><Cards/></div>
-        <div className="StudentTestimonials"><Testimonials/></div>
-        <div className="Contact"><Contact/></div>
-        <div className="Footer"><Footer/></div>
-      </div>
+  render() {
+    const { selectedDate } = this.state;
+    return (
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/contact">
+              <ContactPage />
+            </Route>
+            <Route path="/">
+              <div className="Navigation"><Nav /></div>
+              <div className="Carousel"><Carousel /></div>
+              <div className="Calendar"><Calendar fullDate={selectedDate} onDayClick={this.handleDayClick} /></div>
+              <div className="MentorCards"><Cards /></div>
+              <div className="StudentTestimonials"><Testimonials /></div>
+              <div className="Contact"><Contact /></div>
+              <div className="Footer"><Footer /></div>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
-  handleDayClick(newDay){
-    const {selectedDate}=this.state;
+  handleDayClick(newDay) {
+    const { selectedDate } = this.state;
     this.setState({
       selectedDate: new Date(
         selectedDate.getFullYear(),
         selectedDate.getMonth(),
         newDay
       )
-      })
+    })
   }
 }
 
