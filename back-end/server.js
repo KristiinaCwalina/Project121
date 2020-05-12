0cbc0d01eec9f139b6ef0ae7f12901084d515903
Aleks Andros <1445232@gmail.com>
2020-05-12 20:41:18 +0200

    back-end part-2

--- back-end/server.js
+++ back-end/server.js
@@ -75,77 +75,91 @@ app.post("/mentorsauth", (request, response) => {
 
 app.post("/logout", (request, response) => {
   request.session.mentorsloggedin = false;
-  response.send("You are successfully logged out.")
+  response.send("You have been successfully logged out.");
 });
 
-const students = [
-  {
-    name: "Kristina",
-    surname: "x",
-    class: "oct2019-1",
-    phone: 637953345,
-    email: "kristina@gmail.com"
-  },
-  { name: "Raj", surname: "y", class: "oct2019-1", phone: 638343348, email: "raj@gmail.com" },
-  { name: "Aleks", surname: "z", class: "oct2019-1", phone: 633353345, email: "aleks@gmail.com" }
-];
+app.post("/students", (request, response) => {
+  var name = request.body.name;
+  var student_class = request.body.student_class;
+  var email = request.body.email;
+  var password = request.body.password;
+  var phone = request.body.phone;
 
-const mentors = [
-  {
-    id: 1,
-    name: "Angel",
-    company: "Migracode",
-    skills: "json",
-    email: "angel@gmail.com",
-    password: "hefiehfioehfoeh",
-    phone: 984054785
-  },
-  {
-    id: 2,
-    name: "Sergi",
-    company: "infojobs",
-    skills: "java",
-    email: "sergi@gmail.com",
-    password: "ebfkbf",
-    phone: 948748483
-  },
-  {
-    id: 3,
-    name: "Carlos",
-    company: "Migracode",
-    languages: "c++",
-    email: "carlos@gmail.com",
-    password: "dihfidosf",
-    phone: 848493847
+  if (name && student_class && password && email && phone) {
+    pool.query(
+      "INSERT INTO students (name, student_class, email, password, phone) VALUES ($1, $2, $3, $4,$5)",
+      [name, student_class, email, password, phone],
+      (err, res) => {
+        console.log(err, res);
+        pool.end();
+      }
+    );
+    response.status(201).send("User created and saved to database.");
+  } else {
+    response.status(400).send("Review your requests body.");
   }
-];
+});
+
+app.post("/studentsauth", (request, response) => {
+  var email = request.body.email;
+  var password = request.body.password;
+
+  var parameters = [email, password];
+  if (email && password) {
+    pool.query(
+      "SELECT * FROM students WHERE email = $1 AND password = $2",
+      parameters,
+      (error, results, fields) => {
+        if (results.rowCount > 0) {
+          var userName = results.rows[0].name;
+          request.session.studentsloggedin = true;
+          request.session.username = userName;
+          response.send("Successfully logged in");
+        } else {
+          response.send("Incorrect Username and/or Password!");
+        }
+        response.end();
+      }
+    );
+  } else {
+    response.send("Please enter Username and Password!");
+    response.end();
+  }
+});
+
+app.post("/studentslogout", (request, response) => {
+  request.session.studentsloggedin = false;
+  response.send("You have been successfully logged out.");
+});
 
 app.get("/main", function(req, res) {
   res.send("Welcome");
 });
 
 app.get("/students", function(req, res) {
-  res.send(students);
-});
-
-app.get("/mentors", function(req, res) {
-  let query = `SELECT * FROM mentors`;
+  let query = `SELECT (name, student_class, email) FROM students`;
   pool
     .query(query)
     .then((result) => res.json(result.rows))
     .catch((e) => console.error(e));
 });
 
-app.get("/mentors/:skills", (req, res) => {
-  const languages = req.params.languages;
-  const language = mentors.find((languages) => {
-    return language.languages === languages;
-  });
-  if (!language) {
-    res.send(404);
-  } else {
-    res.send(language);
-  }
+app.get("/mentors", function(req, res) {
+  let query = `SELECT (name, company, skills, email) FROM mentors`;
+  pool
+    .query(query)
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+app.get("/mentors/:skills", (request, res) => {
+  const skills = request.params.skills;
+  let query =
+    "select (name, company, skills, email) from mentors where skills ilike '%' || $1 || '%'";
+  pool
+    .query(query, [skills])
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
 });
 
 app.get("/home", (request, response) => {
